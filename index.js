99bd79107eb875ca225890d75d667abba0abba59
Aman ullah <rapidev.softwares@gmail.com>
2024-03-28 10:25:56 +0500

    db connection

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require("path");
-
+const db = require("./db/connection");
 app.use(express.json());
 app.use(
   cors({
