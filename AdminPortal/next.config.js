68535da4675c74f268624daeaf6d90e35d9c2a70
Aman ullah <rapidev.softwares@gmail.com>
2024-04-01 10:47:30 +0500

    updated matic contract abi and integrated admin panel

---
+++ AdminPortal/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+require("dotenv").config();
+
+module.exports = {
+  output: "export",
+  webpack: (config) => {
+    config.plugins.push(new (require("dotenv-webpack"))());
+    return config;
+  },
+  basePath:"/adminpanel"
+  // Other configurations go here
+};
+// 
